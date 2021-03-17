a35deb2534aaa421343498f9d131a144d6c98ee4
Bernhard Boser <bernhard.e.boser@gmail.com>
2021-03-17 09:36:05 -0700

    extmod/finaliser_proxy: initial commit

--- py/objmodule.c
+++ py/objmodule.c
@@ -230,6 +230,9 @@ STATIC const mp_rom_map_elem_t mp_builtin_module_table[] = {
     #if MICROPY_PY_BLUETOOTH
     { MP_ROM_QSTR(MP_QSTR_ubluetooth), MP_ROM_PTR(&mp_module_ubluetooth) },
     #endif
+    #if MICROPY_PY_FINALISER_PROXY
+    { MP_ROM_QSTR(MP_QSTR_finaliser_proxy), MP_ROM_PTR(&mp_module_finaliser_proxy) },
+    #endif
 
     // extra builtin modules as defined by a port
     MICROPY_PORT_BUILTIN_MODULES
